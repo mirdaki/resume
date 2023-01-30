22321b09e05d832617b364dca7215f9c7a66ce5e
mirdaki <mirdaki@users.noreply.github.com>
2023-01-30 02:33:50 +0000

    Add placeholder content in html

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
 	content: ["./src/**/*.{html,js}"],
 	theme: {
 		extend: {},
+		fontFamily: {
+			'sans': ['inter', 'Helvetica', ...defaultTheme.fontFamily.sans,],
+		}
 	},
 	plugins: [],
 }

e34dba9690fd3bbfc53dc676d73971b11e1485d7
mirdaki <mirdaki@users.noreply.github.com>
2023-02-01 04:46:57 +0000

    Add mobile styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
 	theme: {
 		extend: {},
 		fontFamily: {
-			'sans': ['inter', 'Helvetica', ...defaultTheme.fontFamily.sans,],
+			'sans': ['inter', 'Helvetica'],
 		}
 	},
 	plugins: [],
